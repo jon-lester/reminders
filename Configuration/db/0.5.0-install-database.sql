a5bffd1f3115dd48bd19b434b6804c52a7825ecc
Jon Lester <jon@jonlester.me>
2018-09-21 17:53:08 +0100

    Added db script & guide config files for caddy/systemd in prod environment.

---
+++ Configuration/db/0.5.0-install-database.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS reminders;
+
+DROP TABLE reminders.reminders;
+
+CREATE TABLE IF NOT EXISTS reminders.reminders (
+  ID int(10) unsigned NOT NULL AUTO_INCREMENT,
+  UserID varchar(128) NOT NULL,
+  Title varchar(64) NOT NULL,
+  Description text,
+  ForDate datetime NOT NULL,
+  Created datetime NOT NULL,
+  Recurrence tinyint(4) NOT NULL,
+  Importance tinyint(4) NOT NULL,
+  LastActioned datetime DEFAULT NULL,
+  PRIMARY KEY (ID),
+  KEY idxUserId (UserID)
+) ENGINE=InnoDB;
